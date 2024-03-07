7d3c9726909946870d3b9c4bf61bb52b46fc5fa7
proazad <azadhossain7595.com>
2024-03-08 01:14:00 +0300

    Update Meta Data

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="h-dvh flex justify-center item-center">
+    <main className=" flex justify-center item-center">
       <h1 className="text-4xl text-blue-600">Hello World</h1>
+      <Image src={"https://i.imgur.com/tIKR6B8.jpeg"} alt="dummy" width={400} height={400} />
     </main>
   );
 }
