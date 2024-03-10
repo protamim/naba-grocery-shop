bd2afbf93cf30177d9ac740248cc60c567653017
Tamim Talukdar <s163.tamim@gmail.com>
2024-03-10 13:45:19 +0600

    created banner for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-import Image from 'next/image';
-import React from 'react';
+import BannerHome from '@/components/ui/BannerHome';
+
 
 const HomePage = () => {
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center bg-red-100'>
-      <h3>Welcome to Homepage</h3>
-      <p>Home content goes here...</p>
-      <Image className='h-auto w-auto' src='https://i.imgur.com/tOYJrfH.png' width={350} height={300} alt='demo' priority />
+    <main className=''>
+      <BannerHome />
     </main>
   );
 };
---
+++ src/components/ui/BannerHome.js
@@ -0,0 +1,36 @@
+import { leckerli } from "@/app/layout";
+import { bannerBg, bannerImage } from "@/assets/home";
+import Image from "next/image";
+import ActionButton from "./ActionButton";
+
+const BannerHome = () => {
+  return (
+    <>
+      <div
+        className="min-h-[65vh] relative flex justify-center items-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${bannerBg.src})` }}
+      >
+        <Image
+          className="hidden lg:block mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          src={bannerImage}
+          alt="Naba"
+          width="100%"
+          height="100%"
+          priority
+        />
+        <div className="absolute w-full h-auto pl-4 flex gap-y-3 flex-col items-start">
+          <h4 className={`${leckerli.className} bg-yellow-400 py-2 pl-5 pr-8 entry-subtile text-3xl rounded-md mb-5`}>Garden Fresh Fruits</h4>
+          <h2 className={`${leckerli.className} text-8xl text-black`}>Feel The Taste</h2>
+          <p className="text-lg font-semibold gradient-text max-w-xl mb-4">
+            When an unknown printer took a galley of type and scrambled it to
+            make ype specimen urvived not only five centurieswhen an unknown
+            printer.
+          </p>
+          <ActionButton>Shop Now</ActionButton>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BannerHome;
